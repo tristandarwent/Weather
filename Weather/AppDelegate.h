8591d29c3e51fbbe21e12b27422369f044c6dc1e
Tristan Darwent <tristandarwent@gmail.com>
2019-01-29 14:13:09 -0500

    Initial commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Tristan Darwent on 2019-01-29.
+//  Copyright © 2019 Tristan Darwent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Tristan Darwent on 2019-01-29.
+//  Copyright © 2019 Tristan Darwent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
